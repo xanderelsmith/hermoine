99eccfdda80cb9c40af9898848ab5b7af840c950
xander <xanderelsmith@gmail.com>
2024-04-09 00:39:34 +0100

    fixed quiz

--- lib/src/features/home/presentation/pages/homepage.dart
+++ lib/src/features/home/presentation/pages/homepage.dart
@@ -1,12 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:hermione/src/core/constants/constants.dart';
+import 'package:hermione/src/core/constants/size_utils.dart';
 import 'package:hermione/src/core/global/userdetail.dart';
-import 'package:hermione/src/core/utils/screensizeutils.dart';
-import '../../../../core/widgets/widgets.dart';
-import '../../data/sources/homepagedatasources.dart';
-import '../widgets/bottomnavbar.dart';
 import '../widgets/styledappbar.dart';
 
 class HomePage extends StatelessWidget {
@@ -15,42 +9,101 @@ class HomePage extends StatelessWidget {
   static String id = 'homepage';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const CustomAppBar(),
+    return const Scaffold(
+      appBar: CustomAppBar(),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(8.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text('Course Category'),
-            SizedBox(
-              height: 100,
-              child: ListView.builder(
-                  itemCount: 5,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) => const SizedBox(
-                        height: 100,
-                        width: 100,
-                        child: Card(
-                          child: Column(
-                            children: [
-                              Expanded(
-                                child: CircleAvatar(
-                                  child: Icon(Icons.business),
-                                ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.all(8.0),
-                                child: Text('Science'),
-                              )
-                            ],
-                          ),
-                        ),
-                      )),
-            )
-          ],
+          children: [HomePageCourseCategory(), Courses()],
         ),
       ),
+    );
+  }
+}
+
+class Courses extends StatelessWidget {
+  const Courses({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            child: Text('Courses'),
+          ),
+          Expanded(
+            child: GridView.builder(
+              itemCount: 10,
+              scrollDirection: Axis.horizontal,
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 20,
+                  mainAxisExtent: 200,
+                  maxCrossAxisExtent: 200),
+              itemBuilder: (context, index) => Container(
+                child: Column(
+                  children: [
+                    Expanded(child: SizedBox()),
+                    Expanded(child: SizedBox()),
+                  ],
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10),
+                  color: index.isEven ? Colors.amber : Colors.red,
+                ),
+                height: 200,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomePageCourseCategory extends StatelessWidget {
+  const HomePageCourseCategory({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Course Category'),
+        SizedBox(
+          height: 100,
+          child: ListView.builder(
+              itemCount: 5,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) => const SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: Card(
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: CircleAvatar(
+                              child: Icon(Icons.business),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text('Science'),
+                          )
+                        ],
+                      ),
+                    ),
+                  )),
+        )
+      ],
     );
   }
 }
