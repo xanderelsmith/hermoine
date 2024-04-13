be5f1b73db559c7649cd2111adb71012578a0d61
Wayapay1 <prince.john@wayapaychat.com>
2024-04-13 19:58:57 +0100

    delete user

--- lib/src/features/home/presentation/pages/homepage.dart
+++ lib/src/features/home/presentation/pages/homepage.dart
@@ -1,40 +1,21 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-
 import 'package:cloud_firestore/cloud_firestore.dart';
-
-import 'dart:developer';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'package:hermione/src/core/constants/colors.dart';
 import 'package:hermione/src/core/constants/size_utils.dart';
-
 import 'package:hermione/src/core/widgets/widgets.dart';
-=======
-import 'package:hermione/src/features/assessment/presentation/pages/leaderboard/leaderboard.dart';
-
 import 'package:hermione/src/features/auth/data/models/user.dart';
-import 'package:hermione/src/features/home/domain/repositories/currentuserrepository.dart';
-import 'package:hermione/src/features/home/presentation/widgets/homepage/allcourses.dart';
-import 'package:hermione/src/features/home/presentation/widgets/homepage/allcoursescategoriesListscreen.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
 
 import '../../../auth/presentation/pages/create_account_screen.dart';
-
-import '../../../assessment/presentation/pages/tutor/createdquizscreen.dart';
-import '../../../auth/presentation/pages/auth.dart';
-
 import '../../../auth/presentation/pages/profile.dart';
 import '../../../auth/presentation/pages/signin_screen.dart';
-import '../widgets/customdrawer.dart';
 import '../widgets/homepage/coursecategory.dart';
 import '../widgets/homepage/courseslist.dart';
 import '../widgets/styledappbar.dart';
 
-class HomePage extends ConsumerStatefulWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     super.key,
     this.userDetails,
@@ -43,7 +24,7 @@ class HomePage extends ConsumerStatefulWidget {
   static String id = 'homepage';
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
 final currentUser = FirebaseAuth.instance.currentUser!;
@@ -145,25 +126,11 @@ void deleteUserAccount() async {
         ),
       );
     }
-
-class _HomePageState extends ConsumerState<HomePage> {
-  UserDetails? newUser;
-  @override
-  void initState() {
-    newUser = widget.userDetails;
-    super.initState();
-    fetchUserDetails(FirebaseAuth.instance.currentUser?.email).then((value) {
-      ref.watch(userProvider.notifier).assignUserData(value!);
-      log(value.name.toString());
-      setState(() {
-        newUser = value;
-      });
-    }).onError((error, stackTrace) {
-      log(error.toString());
-    });
-
   }
+}
 
+class _HomePageState extends State<HomePage> {
+  BottomNavItem page = BottomNavItem.home;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -179,13 +146,12 @@ class _HomePageState extends ConsumerState<HomePage> {
                   (index) => InkWell(
                       onTap: () {
                         setState(() {
-                          _page = BottomNavItem.values[index];
+                          page = BottomNavItem.values[index];
                         });
                       },
                       child: Image.asset(BottomNavItem.values[index].data))),
             ),
           )),
-
       body: homePageBuilder(page, widget.userDetails),
     );
   }
@@ -312,9 +278,6 @@ class CustomDrawer extends StatelessWidget {
           ),
         ],
       ),
-
-      body: HomePageBuilder(page: _page, userDetails: newUser!),
-
     );
   }
 }
@@ -330,16 +293,13 @@ class HomeDashboardScreen extends StatelessWidget {
     return Column(
       children: [
         CustomAppBar(userDetails: userDetails),
-        Expanded(
+        const Expanded(
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: EdgeInsets.all(8.0),
             child: Column(
               children: [
-                userDetails.isTutor == true
-                    ? const CreatedQuizes()
-                    : const SizedBox(),
-                const HomePageCourseCategory(),
-                const Courses(),
+                HomePageCourseCategory(),
+                Courses(),
               ],
             ),
           ),
@@ -363,53 +323,30 @@ enum BottomNavItem {
   const BottomNavItem(this.data, this.name);
 }
 
-class HomePageBuilder extends StatelessWidget {
-  const HomePageBuilder(
-      {super.key, required this.page, required this.userDetails});
-  final BottomNavItem page;
-  final UserDetails userDetails;
+Widget homePageBuilder(page, userDetails) {
+  return page == BottomNavItem.home
+      ? HomeDashboardScreen(
+          userDetails: userDetails!,
+        )
+      : page == BottomNavItem.courses
+          ? const Scaffold()
+          : page == BottomNavItem.ranking
+              ? const LeaderBoardRankingScreen()
+              : const Scaffold();
+}
+
+class LeaderBoardRankingScreen extends StatelessWidget {
+  const LeaderBoardRankingScreen({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return page == BottomNavItem.home
-        ? HomeDashboardScreen(
-            userDetails: userDetails,
-          )
-        : page == BottomNavItem.courses
-            ? const AllCoursesScreen()
-            : page == BottomNavItem.ranking
-                ? const LeaderBoardScreen()
-                : const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Leaderboard'),
+        centerTitle: true,
+      ),
+    );
   }
 }
-
-void logout() async {
-  bool confirmLogout = await Get.defaultDialog(
-    title: 'Confirm Logout',
-    middleText: 'Are you sure you want to logout?',
-    actions: [
-      ElevatedButton(
-        onPressed: () {
-          Get.back(result: true); // Return true when confirmed
-        },
-        child: const Text(
-          'Yes',
-          style: TextStyle(color: Colors.black),
-        ),
-      ),
-      ElevatedButton(
-        onPressed: () {
-          Get.back(result: false); // Return false when cancelled
-        },
-        child: const Text('No', style: TextStyle(color: Colors.black)),
-      ),
-    ],
-  );
-
-  if (confirmLogout ?? false) {
-    await FirebaseAuth.instance.signOut();
-    // Navigate to the login screen
-    Get.offAll(const SigninScreen());
-  }
-}
-
-BottomNavItem _page = BottomNavItem.home;
