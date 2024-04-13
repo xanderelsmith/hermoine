ff08d63c37077a412736f1e285527d5353c6b483
xander <xanderelsmith@gmail.com>
2024-04-13 19:20:58 +0100

    fix signi2\nworking code

--- lib/src/features/home/presentation/pages/homepage.dart
+++ lib/src/features/home/presentation/pages/homepage.dart
@@ -1,17 +1,29 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:developer';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:hermione/src/core/constants/colors.dart';
-import 'package:hermione/src/features/auth/data/models/user.dart';
 
+import 'package:hermione/src/core/constants/colors.dart';
+import 'package:hermione/src/core/constants/size_utils.dart';
+import 'package:hermione/src/features/assessment/presentation/pages/leaderboard/leaderboard.dart';
+import 'package:hermione/src/features/auth/data/models/user.dart';
+import 'package:hermione/src/features/home/domain/repositories/currentuserrepository.dart';
+import 'package:hermione/src/features/home/presentation/widgets/homepage/allcourses.dart';
+import 'package:hermione/src/features/home/presentation/widgets/homepage/allcoursescategoriesListscreen.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../../assessment/presentation/pages/tutor/createdquizscreen.dart';
+import '../../../auth/presentation/pages/auth.dart';
 import '../../../auth/presentation/pages/profile.dart';
 import '../../../auth/presentation/pages/signin_screen.dart';
+import '../widgets/customdrawer.dart';
 import '../widgets/homepage/coursecategory.dart';
 import '../widgets/homepage/courseslist.dart';
 import '../widgets/styledappbar.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends ConsumerStatefulWidget {
   const HomePage({
     super.key,
     this.userDetails,
@@ -20,9 +32,114 @@ class HomePage extends StatefulWidget {
   static String id = 'homepage';
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomePageState();
 }
 
+class _HomePageState extends ConsumerState<HomePage> {
+  UserDetails? newUser;
+  @override
+  void initState() {
+    newUser = widget.userDetails;
+    super.initState();
+    fetchUserDetails(FirebaseAuth.instance.currentUser?.email).then((value) {
+      ref.watch(userProvider.notifier).assignUserData(value!);
+      log(value.name.toString());
+      setState(() {
+        newUser = value;
+      });
+    }).onError((error, stackTrace) {
+      log(error.toString());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const CustomDrawer(),
+      bottomNavigationBar: Container(
+          height: 50,
+          color: AppColor.primaryColor,
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: List.generate(
+                  BottomNavItem.values.length,
+                  (index) => InkWell(
+                      onTap: () {
+                        setState(() {
+                          _page = BottomNavItem.values[index];
+                        });
+                      },
+                      child: Image.asset(BottomNavItem.values[index].data))),
+            ),
+          )),
+      body: HomePageBuilder(page: _page, userDetails: newUser!),
+    );
+  }
+}
+
+class HomeDashboardScreen extends StatelessWidget {
+  const HomeDashboardScreen({
+    super.key,
+    required this.userDetails,
+  });
+  final UserDetails userDetails;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomAppBar(userDetails: userDetails),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                userDetails.isTutor == true
+                    ? const CreatedQuizes()
+                    : const SizedBox(),
+                const HomePageCourseCategory(),
+                const Courses(),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+int _selectedIndex = 0;
+
+// Models
+enum BottomNavItem {
+  home('assets/icons/home.png', 'Home'),
+  courses('assets/icons/course.png', 'Courses'),
+  ranking('assets/icons/ranking.png', 'Ranking'),
+  profile('assets/icons/profile.png', 'Profile');
+
+  final String name;
+  final String data;
+  const BottomNavItem(this.data, this.name);
+}
+
+class HomePageBuilder extends StatelessWidget {
+  const HomePageBuilder(
+      {super.key, required this.page, required this.userDetails});
+  final BottomNavItem page;
+  final UserDetails userDetails;
+  @override
+  Widget build(BuildContext context) {
+    return page == BottomNavItem.home
+        ? HomeDashboardScreen(
+            userDetails: userDetails,
+          )
+        : page == BottomNavItem.courses
+            ? const AllCoursesScreen()
+            : page == BottomNavItem.ranking
+                ? const LeaderBoardScreen()
+                : const Scaffold();
+  }
+}
 
 void logout() async {
   bool confirmLogout = await Get.defaultDialog(
@@ -51,195 +168,7 @@ void logout() async {
     await FirebaseAuth.instance.signOut();
     // Navigate to the login screen
     Get.offAll(const SigninScreen());
-
-class _HomePageState extends ConsumerState<HomePage> {
-  UserDetails? newUser;
-  @override
-  void initState() {
-    newUser = widget.userDetails;
-    super.initState();
-    fetchUserDetails(FirebaseAuth.instance.currentUser?.email).then((value) {
-      ref.watch(userProvider.notifier).assignUserData(value!);
-      log(value.name.toString());
-      setState(() {
-        newUser = value;
-      });
-    }).onError((error, stackTrace) {
-      log(error.toString());
-    });
-
   }
 }
 
-class _HomePageState extends State<HomePage> {
-  BottomNavItem page = BottomNavItem.home;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: const CustomDrawer(),
-      bottomNavigationBar: Container(
-          height: 50,
-          color: AppColor.primaryColor,
-          child: Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: List.generate(
-                  BottomNavItem.values.length,
-                  (index) => InkWell(
-                      onTap: () {
-                        setState(() {
-                          page = BottomNavItem.values[index];
-                        });
-                      },
-                      child: Image.asset(BottomNavItem.values[index].data))),
-            ),
-          )),
-      body: homePageBuilder(page, widget.userDetails),
-    );
-  }
-}
-
-class CustomDrawer extends StatelessWidget {
-  const CustomDrawer({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(top: 20),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                CircleAvatar(),
-                Text('name'),
-                Text('email'),
-              ],
-            ),
-          ),
-          Column(
-            children: [
-              InkWell(
-                child: ListTile(
-                  onTap: () {
-                    Get.to(() => ProfileScreen());
-                  },
-                  leading: const Icon(Icons.person, color: Color(0xFF065774)),
-                  title: const Text('Profile'),
-                ),
-              ),
-              const ListTile(
-                leading: Icon(
-                  Icons.lock,
-                  color: Color(0xFF065774),
-                ),
-                title: Text('Security'),
-              ),
-              const ListTile(
-                leading: Icon(Icons.info, color: Color(0xFF065774)),
-                title: Text('About '),
-              ),
-              const ListTile(
-                leading: Icon(Icons.help, color: Color(0xFF065774)),
-                title: Text('Help & Support'),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 25),
-            child: Column(
-              children: [
-                ListTile(
-                  leading: const Icon(Icons.logout_outlined,
-                      color: Color(0xFF065774)),
-                  title: InkWell(
-                      onTap: () {
-                        logout();
-                      },
-                      child: const Text('Log out')),
-                ),
-                const ListTile(
-                  leading: Icon(Icons.delete, color: Color(0xFF065774)),
-                  title: Text('Delete account'),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class HomeDashboardScreen extends StatelessWidget {
-  const HomeDashboardScreen({
-    super.key,
-    required this.userDetails,
-  });
-  final UserDetails userDetails;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CustomAppBar(userDetails: userDetails),
-        const Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                HomePageCourseCategory(),
-                Courses(),
-              ],
-            ),
-          ),
-        )
-      ],
-    );
-  }
-}
-
-int _selectedIndex = 0;
-
-// Models
-enum BottomNavItem {
-  home('assets/icons/home.png', 'Home'),
-  courses('assets/icons/course.png', 'Courses'),
-  ranking('assets/icons/ranking.png', 'Ranking'),
-  profile('assets/icons/profile.png', 'Profile');
-
-  final String name;
-  final String data;
-  const BottomNavItem(this.data, this.name);
-}
-
-Widget homePageBuilder(page, userDetails) {
-  return page == BottomNavItem.home
-      ? HomeDashboardScreen(
-          userDetails: userDetails!,
-        )
-      : page == BottomNavItem.courses
-          ? const Scaffold()
-          : page == BottomNavItem.ranking
-              ? const LeaderBoardRankingScreen()
-              : const Scaffold();
-}
-
-class LeaderBoardRankingScreen extends StatelessWidget {
-  const LeaderBoardRankingScreen({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Leaderboard'),
-        centerTitle: true,
-      ),
-    );
-  }
-}
+BottomNavItem _page = BottomNavItem.home;
