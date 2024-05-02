86e3745190dacfc50086747a2ef5b206fea1a265
xander <xanderelsmith@gmail.com>
2024-05-02 21:42:47 +0100

    geminilast  finish

--- lib/src/core/keys/backendkeys.dart
+++ lib/src/core/keys/backendkeys.dart
@@ -1,5 +1,6 @@
 class ApiPrivateKeys {
-  static const kappId = '';
-  static const kclientKey = '';
+  static const kappId = 'wR6V94SkEpqDfB81syRghvoqiv2hOLQKm0vrNNbt';
+  static const kclientKey = 'T7yquUwVeNVjivcEdHLSgNrgZmpYueH7HXo4XgxA';
   static const keyParseServerUrl = 'https://parseapi.back4app.com';
+  static const openaikey = '';
 }
